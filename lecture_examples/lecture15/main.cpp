f37ce4b8e09dc6ec177e9b8a2a232511116ab5d8
asen-krasimirov <personalgrowth57@gmail.com>
2023-06-19 12:07:21 +0300

    adding Builder design pattern example

--- lecture_examples/lecture15/main.cpp
+++ lecture_examples/lecture15/main.cpp
@@ -1,13 +1,17 @@
 //#include "DesignPatterns/Command/Command.h"
 
-#include "SingletonFactory/SingletonFactory.h"
+//#include "SingletonFactory/SingletonFactory.h"
+
+#include "DesignPatterns/Builder/Builder.h"
 
 int main() {
-    auto factory = SingletonFactory::getInstance();
+//    auto factory = SingletonFactory::getInstance();
 
-    auto ptr1 = factory->create(BaseType::Der1);
-    auto ptr2 = factory->create(BaseType::Der2);
-    auto ptr3 = factory->create(BaseType::Der3);
+//    auto ptr1 = factory->create(BaseType::Der1);
+//    auto ptr2 = factory->create(BaseType::Der2);
+//    auto ptr3 = factory->create(BaseType::Der3);
 
-    factory->freeInstance();
+//    factory->freeInstance();
+
+    testMain();
 }
