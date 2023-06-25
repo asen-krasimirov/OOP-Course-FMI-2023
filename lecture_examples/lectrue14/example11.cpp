fe4d74870c857606aa967c55196a0c8d27774996
asen-krasimirov <personalgrowth57@gmail.com>
2023-06-25 17:46:09 +0300

    renaming "lectrue14" to "lecture14"

--- lecture_examples/lectrue14/example11.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-struct E {
-    virtual ~E() = default;
-};
-
-//int main() {
-//    std::cout << sizeof(E);
-//}
--- lecture_examples/lectrue14/example13.cpp
+++
@@ -1,6 +0,0 @@
-//#include<iostream>
-//
-//int main() {
-//    UniquePtr p1(new int(10));
-//    UniquePtr p2(p1);
-//}
